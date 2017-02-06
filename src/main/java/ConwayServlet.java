a9e22e53d65fb08e9c33ff2ee5db7c727d0518f5
Stephen Lander <stephenclander@gmail.com>
2017-02-06 17:26:47 -0500

    setup base servlet

---
+++ src/main/java/ConwayServlet.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class ConwayServlet extends HttpServlet {
+
+    public void init() {
+
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher view = request.getRequestDispatcher("conway.html");
+        view.forward(request, response);
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
