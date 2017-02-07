5bb6b2bae862c32f94c27cdc517dee1627cadbd3
Stephen Lander <stephenclander@gmail.com>
2017-02-06 21:33:06 -0500

    angular view, styling and JS data

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -1,5 +1,21 @@
 var app = angular.module('app', []);
 
 app.controller('ConwayCtrl', function($scope) {
-    $scope.message = 'Angular works now';
+    var numRows = 6;
+    var numCols = 8;
+
+    // If new
+    var emptyRow = new Array(numCols).fill(false);
+    $scope.rows = new Array(numRows).fill(emptyRow);
+
+    $scope.setCell = function(isAlive, colIndex, rowIndex) {
+        var row = angular.copy($scope.rows[rowIndex]);
+        row[colIndex] = isAlive;
+        $scope.rows[rowIndex] = row;
+    }
+
+    $scope.nextRound = function() {
+        console.log('data', $scope.rows);
+    }
+
 });
