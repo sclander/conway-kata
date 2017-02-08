3f59f3c076f775709258b525bcfeb2a29d24801b
Stephen Lander <stephenclander@gmail.com>
2017-02-07 21:41:19 -0500

    added grid size controls

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -1,14 +1,26 @@
 var app = angular.module('app', []);
 
 app.controller('ConwayCtrl', function($scope, $http, $interval) {
+    // initial values
     var numRows = 6;
     var numCols = 8;
     var animationPromise;
     $scope.animating = false;
+    $scope.displayNumRows = numRows;
+    $scope.displayNumCols = numCols;
 
     // initialize new grid
-    var emptyRow = new Array(numCols).fill(false);
-    $scope.rows = new Array(numRows).fill(emptyRow);
+    $scope.initGrid = function(newNumRows, newNumCols) {
+        if($scope.animating) {
+            $scope.stopAnimation();
+        }
+        var emptyRow = new Array(newNumCols).fill(false);
+        $scope.rows = new Array(newNumRows).fill(emptyRow);
+        numRows = newNumRows;
+        numCols = newNumCols;
+    }
+
+    $scope.initGrid(numRows, numCols);
 
     // click handler function to change cell state
     $scope.setCell = function(isAlive, colIndex, rowIndex) {
@@ -37,6 +49,7 @@ app.controller('ConwayCtrl', function($scope, $http, $interval) {
         });
     }
 
+    // start animation - sets interval for post every 500ms
     $scope.startAnimation = function() {
         animationPromise = $interval(function() {
             $scope.nextRound();
@@ -44,6 +57,7 @@ app.controller('ConwayCtrl', function($scope, $http, $interval) {
         }, 500);
     }
 
+    // stop animation - cancels post interval
     $scope.stopAnimation = function() {
         $interval.cancel(animationPromise);
         $scope.animating = false;
